d0a3be314ca5907261ab5b6a9d82aeee51b9cf88
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-28 19:54:39 +0000

    Within the user registration post request, have turned the cb into async/await and using try/catch. the catch for server errors console.errors the err.message value, and sends response of status 500 to indicate server error and sends text 'Server error'. Within the try, first thing trying to do is see if user exists in the User database. Brought in the user model, and using findOne() query with email as an argument for the query, search to see if a user is returned. If user is returned, response with status 400 indiciating request error and and JSON error message of 'User already exists' is sent

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
 const { check, validationResult } = require('express-validator/check')
+const User = require('../../models/User')
 
 // @route POST api/users
 // @description register user
@@ -9,12 +11,21 @@ router.post('/', [
   check('name', 'Please enter a name').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array()})
   }
-  res.send("User Registration")
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email: email })
+    if(user) { res.status(400).json({errors: [ {msg: 'User already exists'}]})}
+    res.send("User Registration")
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error');
+  }
 })
 
 module.exports = router;
