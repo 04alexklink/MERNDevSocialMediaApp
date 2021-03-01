c0769720def3abe679b053f186e7f3653755e15b
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-01 10:43:15 +0000

    Created a log in route that validates email and password fields are as expected in the post request. Then checks if email can be found in Users collection db, if not, returns error message as json saying Invalid credentials. If email is found, then use bcrypt compare method that checks password sent in the post request is the same as the one associated with the user found from the db by the email. If passwords match, create a token and send it as json response. If passwords don't match, send error message saying Invalid credentials. Authentication for backend now complete. Can sign up, log in, protect routes and get tokens

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator/check')
 
 // @route GET api/auth
 // @description authentication route
@@ -17,5 +21,40 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send("Server Error")
   }
 })
+
+// @route POST api/auth
+// @description Authenticate user and get token
+// @access PUBLIC
+router.post('/', [
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()})
+  }
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email: email })
+    if(!user) { return res.status(400).json({errors: [ {msg: 'Invalid Credentials'}]})}
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if(!isPasswordMatch) { return res.status(400).json({errors: [ {msg: 'Invalid Credentials'}]})}
+    const payload = { user: {
+        id: user.id
+    }}
+    jwt.sign(payload, config.jwtSecret, {
+      expiresIn: 360000
+    },
+    (err, token) => {
+      if(err) throw err;
+      res.json({ token });
+    })
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error');
+  }
+})
+
 
 module.exports = router;
