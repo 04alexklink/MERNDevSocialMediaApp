ad2d0468d38251221306a1afad80b2b87039528f
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-01 09:29:15 +0000

    Auth route uses this middleware authentication module within the get request, inserted as a 2nd paramater. Within this auth middleware fn, the token (if present and valid, coming from the request) is decoded, and res.user is set to the id value that was stored as a payload within this token, that's now visible after being decoded. The (req/res) fn then uses a try catch, with the try part calling findByID on the user model, passing in this req.user.id in order to search the User collection for the user, and if found returns the user data as json, minus the password. If there's any error, the error is logged and response is status 500 and message of Server Error

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 
 // @route GET api/auth
-// @description authentication test route
+// @description authentication route
 // @access PUBLIC
-router.get('/', (req, res) => {
-  res.send("Authentication route");
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+    .select('-password');
+    res.json(user);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error")
+  }
 })
 
 module.exports = router;
