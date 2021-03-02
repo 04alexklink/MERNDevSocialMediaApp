ad527bc832bc420fd470ba7c668a8a8407b8bca7
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-02 10:34:00 +0000

    Added a put route for creating an experience and adding it to an existing users profile. Checks for validation errors e.g. need a title, company and from date as required fields and returns errors if these aren't provided. Then Pulls out the fields from req.body and use them to create a new experience object. Then pull the users profile out from Profile collection and add the new experience object to the profile.experience array. The response is then the profile, which will contain the new experience that was added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -123,4 +123,48 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+// @route PUT api/profile/experience
+// @description add profile experience
+// @access PRIVATE
+router.put('/experience', [auth,[
+  check('title', 'Title is required').not().isEmpty(),
+  check('company', 'Company is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty()
+]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+  const {
+    title,
+    company, 
+    location, 
+    from, 
+    to, 
+    current, 
+    description} = req.body
+
+  const newExp = {
+    //title = title
+    title, 
+    company, 
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+  try{
+  const profile = await Profile.findOne({user: req.user.id})
+  // what happens if user adds experience without having created a profile first? Edge case
+  // in this case profile wont' be found, and the below adding experience won't work
+  profile.experience.unshift(newExp)
+  await profile.save();
+  res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 module.exports = router;
