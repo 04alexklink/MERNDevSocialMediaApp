586d55a0f2e0dd7b2d7ae2a698d1ffa028dc1d40
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-02 18:59:21 +0000

    get post by post id route created and works. Simply extracts the post id from the http request and uses it to search the post model using mongoose method findById(req.params.id). This return value is set to variable post. If post is false return error message with status 404 as means post was not found. If post exists, send the post as json. Tested route on postman and got back the post whose post id i included in the http request. Note this is a private route so must include a token in the header when making the request

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,12 +35,25 @@ router.post('/', [auth, [
 
 // @route GET api/posts
 // @description get all posts
-// @access PUBLIC
-router.get('/', async (req, res) => {
+// @access Private
+router.get('/', auth, async (req, res) => {
   try {
-    const posts = await Post.find()
+    const posts = await Post.find().sort({date: -1})
     res.json(posts)
   } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+})
+// @route GET api/posts/:id
+// @description get post by id
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if(!post) {return res.status(400).json({msg: 'This post does not exist'})}
+    res.json(post)
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
