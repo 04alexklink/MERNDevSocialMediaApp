4adec7eb323bfca5dc512a4acf372d1bb49374f4
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-02 13:00:52 +0000

    Add education route and delete education route added and tested in Postman. Both work in same way as experience routes for add experience and delete experience work

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -186,4 +186,66 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+// @route PUT api/profile/education
+//@ description add education to a profile
+// @ access Private
+router.put('/education', [auth,[
+  check('school', 'School is required').not().isEmpty(),
+  check('degree', 'Degree is required').not().isEmpty(),
+  check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newEduc = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  }
+  try{
+    let profile = await Profile.findOne({user: req.user.id})
+    profile.education.unshift(newEduc)
+    await profile.save()
+    res.json(profile)
+  }
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+})
+// @route DELETE api/profile/education/:edu_id
+// @description delete a profile education
+// @access PRIVATE
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try{
+  const profile = await Profile.findOne({user: req.user.id})
+  // const removeIndex = profile.education.map((education) => education.id).indexOf(req.params.edu_id)
+  // profile.education.splice(removeIndex, 1)
+  const updatedProfileEducations= profile.education.filter((education) => {
+    if(education.id !== req.params.edu_id) return education
+  })
+  profile.education = updatedProfileEducations
+  await profile.save();
+  res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 module.exports = router;
