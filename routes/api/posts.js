cd723710bb336bf080ae1a9e6f32c24174eea1c6
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-02 18:33:07 +0000

    Created post route for creating a post. Authentication middleware and validation middleware are used to check token is sent in header and that text is a field of the JSON sent in the body as required to create a post. Then the user id that is in req.user.id by the authentication process is used to find the specific user making the post request in the User model. Then a new post object is created with the text from the req body, and then name, avatar, and user id from the user instance variable. This new post object is then passed as a paramater to the new Post call to the model which returns the new post instance, and this is returned in json. Second route created is a get all posts route which just sets a variable posts to Post.find(), so get's all the documents in the posts collection in MongoDB Atlas and this posts variable (array of post objects) is sent back in json. Both routes tested in Postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const Post = require('../../models/Post')
+const User = require('../../models/User')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator/check')
+
+// @route POST api/posts
+// @description add a post
+// @access Private
+router.post('/', [auth, [
+ check('text', 'Text in a post is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+  try{
+  const user = await User.findById(req.user.id)
+  const newPostFields = {
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id
+  }
+  const post = await new Post(newPostFields);
+  await post.save()
+  res.json(post)
+  } catch(err) {
+  console.error(err.message)
+  res.status(500).send('Server Error')
+  }
+})
+
 
 // @route GET api/posts
-// @description posts test route
+// @description get all posts
 // @access PUBLIC
-router.get('/', (req, res) => {
-  res.send("Posts route");
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
 })
 
 module.exports = router;
