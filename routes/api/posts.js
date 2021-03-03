000f574ac688a3b7578f5689d684564f75a69ca2
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-03 13:12:19 +0000

    Add comments to posts route created. If this post route to api/posts/comment/:id is used to make a request, by a user who is logged in, if in the request body there is text, the request will pass the authentication middleware and error check on params middleware and in try fn, will use the user id from the token in req.user.id to find the user making the comment from the User model. Will also find the post being commented on (known from the post id in the header and accessed via req.params.id). Then create a newComment object using text from the body of the request as the text value, name and avatar values coming from the user document, and user id field using req.user.id. Once this new comment object is created, insert it into post.comments array and save the post to the database. Send as json post.comments (all the comments on the post being commented on). Tested this route on postman and all works

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -110,4 +110,33 @@ router.put('/unlike/:id', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+// @route POST api/posts/comment/:id
+// @description comment on a post
+// @access Private
+router.post('/comment/:id', [auth, [
+check('text', 'Comments must contain text').not().isEmpty() ]],async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()})
+    }
+  try {
+    //find post to comment on from the header :id
+  const post = await Post.findById(req.params.id)
+  // find user whose logged in and making the request to comment
+  const user = await User.findById(req.user.id).select('-password')
+  // create new comment object
+  const newComment = {
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id
+  }
+  post.comments.unshift(newComment)
+  await post.save()
+  res.json(post.comments)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+})
 module.exports = router;
