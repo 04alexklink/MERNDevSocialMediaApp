aeb81e606832ddf57bbfe794fb86356115381f3f
04alexklink <alex_klink123@hotmail.co.uk>
2021-03-11 21:18:12 +0000

    Register component. Set component level state and onChange handlers called in each of 4 form input types (email, name, pw, pw2), set equal to onChange function. In this fn, setstate for that particular attribute (e.target.name) and set equal to e.target.value). Created onSubmit fn in the form that for now just outputs the form data state to the console and resets the 4 attributes in the formData state to empty strings so the fields clear

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
-import Register from './components/auth/Register'
+import Register from './components/auth/Register';
 
 const App = () => (
   <Router>
